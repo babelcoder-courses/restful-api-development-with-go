59088d3a36444ee310a357e5c329a29d05fa8224
Nuttavut Thongjor <n.thongjor@gmail.com>
2020-08-06 19:21:18 +0700

    Query and Params

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,23 +14,41 @@ import (
 // DELETE /api/v1/articles/:id
 
 type article struct {
+	ID    uint
 	Title string
 	Body  string
 }
 
 func Serve(r *gin.Engine) {
 	articles := []article{
-		{Title: "Title#1", Body: "Body#1"},
-		{Title: "Title#2", Body: "Body#2"},
-		{Title: "Title#3", Body: "Body#3"},
-		{Title: "Title#4", Body: "Body#4"},
-		{Title: "Title#5", Body: "Body#5"},
+		{ID: 1, Title: "Title#1", Body: "Body#1"},
+		{ID: 2, Title: "Title#2", Body: "Body#2"},
+		{ID: 3, Title: "Title#3", Body: "Body#3"},
+		{ID: 4, Title: "Title#4", Body: "Body#4"},
+		{ID: 5, Title: "Title#5", Body: "Body#5"},
 	}
+
 	articlesGroup := r.Group("/api/v1/articles")
 	articlesGroup.GET("", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"articles": articles})
+		result := articles
+		if limit := ctx.Query("limit"); limit != "" {
+			n, _ := strconv.Atoi(limit)
+
+			result = result[:n]
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"articles": result})
 	})
 	articlesGroup.GET("/:id", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Hello, Thailand")
+		id, _ := strconv.Atoi(ctx.Param("id"))
+
+		for _, item := range articles {
+			if item.ID == uint(id) {
+				ctx.JSON(http.StatusOK, gin.H{"article": item})
+				return
+			}
+		}
+
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Article not found"})
 	})
 }
